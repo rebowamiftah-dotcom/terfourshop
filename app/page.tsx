d8839515a55bf7af894d3a9558ded280335c4ed9
fakhri-fullstack <ernif2.ed@gmail.com>
2026-08-10 13:40:16 +0700

    Fakhri menambahkan komponen navbar dan halaman about

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./components/Navbar";
+
+export default function HomePage() {
+    return (
+      <main className="min-h-screen bg-slate-950">
+        <Navbar />
+      </main>
+    );
+}
